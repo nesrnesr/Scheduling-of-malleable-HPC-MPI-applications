72077d81bcca28ea864795c74aeffbb63929c61b
Ness <arnoub.nemu@gmail.com>
2020-03-27 10:45:02 +0100

    Update_schedule: looks for potentially reconfigurable tasks

--- Naive greedy alg/Scheduler.py
+++ Naive greedy alg/Scheduler.py
@@ -41,6 +41,13 @@ class Scheduler(object):
                     available_servers = self._available_servers(time + 0.01)
                     num_available_servers = len(available_servers)
 
+        #find tasks that could be reconfigured
+        #List of tasks for which the possible change of the number of servers is greater than 0
+        tasks_candidates = [
+            t for t in self.tasks if
+            self._task_possible_inc_num_ser(t, time, num_available_servers) > 0
+        ]
+
     # returns a list of servers not utilized at a given time
     def _available_servers(self, time):
         #Start with all servers as potential servers
@@ -60,3 +67,18 @@ class Scheduler(object):
         exec_time = self._exec_time(job.mass, job.alpha, num_servers)
         self.tasks.append(
             Task(job.id, job.mass, servers, time, time + exec_time))
+
+    # Returns the possible increase in the number of servers for a task
+    # given that num_servers are not busy
+    def _task_possible_inc_num_ser(self, task, time, num_servers):
+        #job = list(filter(lambda j: (j.id == task.job_id), self.jobs))[0]
+        if (task.end_time <= time):
+            return 0
+        job = next(j for j in self.jobs if (j.id == task.job_id))
+        task_num_servers = len(task.servers)
+        if (task_num_servers == job.max_num_servers):
+            return 0
+        elif (task_num_servers + num_servers > job.max_num_servers):
+            return job.max_num_servers - task_num_servers
+        else:
+            return num_servers
