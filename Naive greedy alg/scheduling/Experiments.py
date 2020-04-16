4c4cd81b7913925578f0739b868a64e9c6d09709
Ness <arnoub.nemu@gmail.com>
2020-04-16 21:42:06 +0200

    Added means of drawing gantt chart and changed parameters ranges

--- Naive greedy alg/scheduling/Experiments.py
+++ Naive greedy alg/scheduling/Experiments.py
@@ -1,29 +1,23 @@
-from .Job import Job
-from .Server import Server
-from .Scheduler import Scheduler, SchedulerConfig
+import sys
+from math import ceil
+from random import randrange, seed, uniform
+
 import pandas as pd
-from random import seed, uniform, randrange
+
+from .JobRequest import JobRequest
+from .Scheduler import Scheduler, SchedulerConfig
+from .Server import Server
+from .Visualizer import Visualizer
 
 
 class Experiments:
-    STAT_COLS = [
-        "num reconfig",
-        "num power off",
-        "min stretch",
-        "max stretch",
-        "mean stretch",
-        "std stretch",
-        "av power",
-        "cost function",
-    ]
-
     def __init__(self, seed):
         self.seed = seed
 
     def make_random_config(self):
         seed(self.seed)
         c = SchedulerConfig()
-        c.server_threshold = uniform(0.5, 0.91)
+        c.server_threshold = uniform(0.05, 0.2)
         c.ratio_almost_finished_jobs = uniform(0.5, 0.91)
         c.time_remaining_for_power_off = uniform(370, 600)
         c.shut_down_time = uniform(
@@ -48,34 +42,75 @@ class Experiments:
         return pd.concat(stats)
 
     def _run_expt(self, config):
-        servers = [Server("server" + str(i)) for i in range(10)]
+        servers = [Server("server" + str(i)) for i in range(8)]
         scheduler = Scheduler(servers, config)
-        jobs = self._generate_jobs()
+        jobs = self._generate_jobs(30, len(servers))
 
-        for t in range(18000):
-            time = t * 10
-            for job in jobs:
-                if job.sub_time > time:
+        time = 0
+        while jobs or scheduler.is_working():
+            # print(f"time: {time}, {jobs}")
+            # sys.stdin.read(1)
+            for job in list(jobs):
+                if time < job.sub_time:
                     break
-                scheduler.schedule1(job)
+                scheduler.schedule(job)
                 jobs.remove(job)
-            scheduler.update_schedule1(time)
+            scheduler.update_schedule(time)
+            time += 10
 
-        return pd.DataFrame([scheduler.stats()], columns=self.STAT_COLS)
+        stats = scheduler.stats()
+        visualizer = Visualizer()
+        visualizer.draw_gantt(servers, stats, "result.png")
 
-    def _generate_jobs(self):
-        jobs = [self._generate_job(i) for i in range(30)]
+        stat_dict = stats.to_dict()
+        return pd.DataFrame(stat_dict, index=[0])
+
+    # def _generate_jobs(self, _, server_count):
+    #     a = JobRequest(
+    #         id="A",
+    #         sub_time=100,
+    #         alpha=1,
+    #         data=10,
+    #         mass=10,
+    #         min_num_servers=2,
+    #         max_num_servers=server_count,
+    #     )
+    #
+    #     b = JobRequest(
+    #         id="B",
+    #         sub_time=101,
+    #         alpha=1,
+    #         data=10,
+    #         mass=100,
+    #         min_num_servers=2,
+    #         max_num_servers=3,
+    #     )
+    #
+    #     c = JobRequest(
+    #         id="C",
+    #         sub_time=102,
+    #         alpha=1,
+    #         data=10,
+    #         mass=300,
+    #         min_num_servers=1,
+    #         max_num_servers=3,
+    #     )
+    #
+    #     return [a, b, c]
+
+    def _generate_jobs(self, job_count, server_count):
+        jobs = [self._generate_job(server_count, i) for i in range(job_count)]
         jobs = sorted(jobs, key=lambda k: k.sub_time)
         return jobs
 
-    def _generate_job(self, num):
+    def _generate_job(self, server_count, num):
         sub_time = uniform(0, 3000)
         alpha = uniform(0.5, 1)
-        data = uniform(10, 1000)
-        mass = uniform(10, 50000)
-        min_num_servers = randrange(1, 9)
-        max_num_servers = randrange(min_num_servers, 10)
-        return Job(
+        data = uniform(10, 100)
+        mass = uniform(10, 5000)
+        min_num_servers = ceil(alpha * (server_count - 1))
+        max_num_servers = randrange(min_num_servers, server_count)
+        return JobRequest(
             "job" + str(num),
             sub_time,
             alpha,
--- Naive greedy alg/scheduling/__init__.py
+++ Naive greedy alg/scheduling/__init__.py
@@ -5,6 +5,7 @@ def main():
     experiments = Experiments(seed=2)
     config = experiments.make_random_config()
     stats = experiments.run_expts(config, num_expts=2)
+
     print("Experiment stats:\n", stats)
-    print("Mean cost: ", stats["cost function"].mean())
+    print("Mean cost: ", stats["cost"].mean())
     print("Tuning configs:\n", config)
