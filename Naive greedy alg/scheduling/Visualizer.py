0d9bb31dc375617b98271959f2ab0659856c3379
briag <briagd@yahoo.com>
2020-04-27 13:48:26 +0200

    Added the ability to get a convergence graph from the swarm

--- Naive greedy alg/scheduling/Visualizer.py
+++ Naive greedy alg/scheduling/Visualizer.py
@@ -47,6 +47,32 @@ class Visualizer:
         plt.savefig(filepath, dpi=200)
         plt.close(filepath)
 
+    def draw_graph(self, stats, filepath):
+        path = Path(filepath)
+        path.parent.mkdir(0o755, parents=True, exist_ok=True)
+        fig, ax = plt.subplots(1)
+        ax.plot(
+            stats["epoch"], stats["mean_cost"], lw=2, label="mean fitness", color="blue"
+        )
+        ax.fill_between(
+            stats["epoch"],
+            stats["min_cost"],
+            stats["max_cost"],
+            facecolor="blue",
+            alpha=0.1,
+        )
+        ax.fill_between(
+            stats["epoch"],
+            stats["mean_cost"] + stats["std_cost"],
+            stats["mean_cost"] - stats["std_cost"],
+            facecolor="blue",
+            alpha=0.4,
+        )
+        ax.legend(loc="upper right")
+        ax.set_xlabel("Epoch")
+        ax.set_ylabel("Cost")
+        plt.savefig(filepath, dpi=200)
+
     def _draw_rectangle(self, tl, size, color):
         rectangle = plt.Rectangle((tl.x, tl.y), size.x, size.y, fc=astuple(color))
         plt.gca().add_patch(rectangle)
