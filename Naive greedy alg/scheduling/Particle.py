9697237a2147304892d36281c9c19dd6ddc887e1
briag <briagd@yahoo.com>
2020-04-27 13:46:12 +0200

    Updated how particles behave at bounds using reflection

--- Naive greedy alg/scheduling/Particle.py
+++ Naive greedy alg/scheduling/Particle.py
@@ -20,7 +20,7 @@ class Particle:
         best_pos = np.array(self.best_config.to_list())
         group_best_pos = np.array(group_best_config.to_list())
 
-        self.velocity = (
+        self.velocity = 0.1 * (
             self.velocity
             + self.c1 * random() * (best_pos - position)
             + self.c2 * random() * (group_best_pos - position)
@@ -39,38 +39,60 @@ class Particle:
             self.best_config = self.config
 
     def _check_bounds(self, config):
-        config.shutdown_prob = min(max(config.shutdown_prob, 0.05), 0.91)
-        config.shutdown_weight = min(max(config.shutdown_weight, 0.05), 0.91)
-        config.reconfig_prob = min(max(config.reconfig_prob, 0.05), 0.91)
-        config.reconfig_weight = min(max(config.reconfig_weight, 0.05), 0.91)
-        config.shutdown_time = max(config.shutdown_time, 360)
+        config.shutdown_prob = self._reflect(config.shutdown_prob, 0, 1)
+        config.shutdown_weight = self._reflect(config.shutdown_weight, 0, 1)
+        config.reconfig_prob = self._reflect(config.reconfig_prob, 0, 1)
+        config.reconfig_weight = self._reflect(config.reconfig_weight, 0, 1)
+        config.shutdown_time_1 = self._reflect(config.shutdown_time_1, 260, 100000)
+        config.shutdown_time_2 = self._reflect(config.shutdown_time_2, 260, 100000)
+        config.shutdown_time_prob = self._reflect(config.shutdown_time_prob, 0, 1)
+        config.alpha_min_server_lower_range = self._reflect(
+            config.alpha_min_server_lower_range, 0, 1
+        )
+        config.alpha_min_server_mid_range = self._reflect(
+            config.alpha_min_server_mid_range, config.alpha_min_server_lower_range, 1
+        )
+        config.alpha_min_server_upper_range = self._reflect(
+            config.alpha_min_server_upper_range, config.alpha_min_server_mid_range, 1
+        )
+        config.alpha_lower = self._reflect(config.alpha_lower, 0.5, 1)
+        config.alpha_mid = self._reflect(config.alpha_mid, config.alpha_lower, 1)
 
-        # config.alpha_min_server_lower_range = min(max(config.alpha_min_server_lower_range, 0.01), 0.9)
-        # config.server_threshold = min(max(config.server_threshold, 0.05), 0.9)
-        # config.server_threshold = min(max(config.server_threshold, 0.05), 0.9)
-
-        if config.alpha_min_server_lower_range < 0:
-            config.alpha_min_server_lower_range = 0.01
-        if config.alpha_min_server_lower_range > 1:
-            config.alpha_min_server_lower_range = 0.91
-        if config.alpha_min_server_mid_range < config.alpha_min_server_lower_range:
-            config.alpha_min_server_mid_range = (
-                config.alpha_min_server_lower_range + 0.1
-            )
-        if config.alpha_min_server_mid_range > 1:
-            config.alpha_min_server_mid_range = 0.91
-        if config.alpha_min_server_upper_range < config.alpha_min_server_mid_range:
-            config.alpha_min_server_upper_range = (
-                config.alpha_min_server_mid_range + 0.1
-            )
-        if config.alpha_min_server_upper_range > 1:
-            config.alpha_min_server_upper_range = 0.99
-        if config.alpha_lower < 0.5:
-            config.alpha_lower = 0.5
-        if config.alpha_lower > 1:
-            config.alpha_lower = 0.91
-        if config.alpha_mid < config.alpha_lower:
-            config.alpha_mid = config.alpha_lower + 0.01
-        if config.alpha_mid > 1:
-            config.alpha_mid = 0.99
+        # config.shutdown_prob = min(max(config.shutdown_prob, 0.000001), 0.999999)
+        # config.shutdown_weight = min(max(config.shutdown_weight, 0.000001), 0.999999)
+        # config.reconfig_prob = min(max(config.reconfig_prob, 0.000001), 0.999999)
+        # config.reconfig_weight = min(max(config.reconfig_weight, 0.000001), 0.999999)
+        # config.shutdown_time_1 = max(config.shutdown_time_1, 10)
+        # config.shutdown_time_2 = max(config.shutdown_time_2, 10)
+        # config.shutdown_time_prob = min(
+        #     max(config.shutdown_time_prob, 0.000001), 0.999999
+        # )
+        # config.alpha_min_server_lower_range = min(
+        #     max(config.alpha_min_server_lower_range, 0.000001), 0.91
+        # )
+        # config.alpha_min_server_mid_range = min(
+        #     max(
+        #         config.alpha_min_server_mid_range,
+        #         config.alpha_min_server_lower_range + 0.001,
+        #     ),
+        #     0.91,
+        # )
+        # config.alpha_min_server_upper_range = min(
+        #     max(
+        #         config.alpha_min_server_upper_range,
+        #         config.alpha_min_server_mid_range + 0.001,
+        #     ),
+        #     0.99,
+        # )
+        # config.alpha_lower = min(max(config.alpha_lower, 0.5), 0.91)
+        # config.alpha_mid = min(
+        #     max(config.alpha_mid, config.alpha_lower + 0.001,), 0.99,
+        # )
         return config
+
+    def _reflect(self, variable, lowerbound, upperbound):
+        if variable > upperbound:
+            return upperbound - (variable - upperbound)
+        if variable < lowerbound:
+            return lowerbound + (lowerbound - variable)
+        return variable
