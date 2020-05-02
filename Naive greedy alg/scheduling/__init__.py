5e7edb7e1466d3d1eab81b2570a94ec37b40cd72
briag <briagd@yahoo.com>
2020-05-02 18:52:04 +0200

    Added the different set up for each experiments to be conducted in one run

--- Naive greedy alg/scheduling/__init__.py
+++ Naive greedy alg/scheduling/__init__.py
@@ -1,6 +1,11 @@
 import logging
+import pandas as pd
 
-from .Swarm import Swarm
+# from .Swarm import Swarm
+from .Experiments import Experiments
+from .Scheduler import SchedulerConfig
+
+# python -m scheduling
 
 
 def main():
@@ -8,7 +13,51 @@ def main():
         level=logging.INFO, format="%(asctime)s %(name)s %(levelname)-5s: %(message)s"
     )
 
-    swarm = Swarm(seed_num=1, num_particles=10, num_srvs=10, num_exp=10)
-    # swarm.configs()
-    swarm.run_epochs(num_epochs=1, draw_stats=True)
-    # swarm.configs()
+    # swarm = Swarm(seed_num=1, num_particles=30, num_srvs=10, num_exp=50)
+    # # swarm.configs()
+    # swarm.run_epochs(num_epochs=100, draw_stats=False, draw_graph=True)
+    # # swarm.configs()
+
+    n_servers = 10
+    n_expts = 30
+    seed = 2
+    # run expt with no reconfig and no power off
+    experiment = Experiments(
+        reconfig_enable=False, power_off_enable=False, param_enable=False
+    )
+    configuration = SchedulerConfig
+    stats = experiment.run_expts(
+        config=configuration, num_srvs=n_servers, num_expts=n_expts, seed_num=seed
+    )
+    stats_pd = pd.DataFrame([stat.to_dict() for stat in stats])
+    stats_pd.to_csv("all_false_stats.csv")
+    # run expt with reconfig and no power off
+    experiment = Experiments(
+        reconfig_enable=True, power_off_enable=False, param_enable=False
+    )
+    configuration = SchedulerConfig
+    stats = experiment.run_expts(
+        config=configuration, num_srvs=n_servers, num_expts=n_expts, seed_num=seed
+    )
+    stats_pd = pd.DataFrame([stat.to_dict() for stat in stats])
+    stats_pd.to_csv("reconfig_stats.csv")
+    # run expt with no reconfig and power off
+    experiment = Experiments(
+        reconfig_enable=False, power_off_enable=True, param_enable=False
+    )
+    configuration = SchedulerConfig
+    stats = experiment.run_expts(
+        config=configuration, num_srvs=n_servers, num_expts=n_expts, seed_num=seed
+    )
+    stats_pd = pd.DataFrame([stat.to_dict() for stat in stats])
+    stats_pd.to_csv("powerOff_stats.csv")
+    # run expt with reconfig and power off and param from swarm
+    experiment = Experiments(
+        reconfig_enable=True, power_off_enable=True, param_enable=True
+    )
+    configuration = SchedulerConfig
+    stats = experiment.run_expts(
+        config=configuration, num_srvs=n_servers, num_expts=n_expts, seed_num=seed
+    )
+    stats_pd = pd.DataFrame([stat.to_dict() for stat in stats])
+    stats_pd.to_csv("param_stats.csv")
